2220dd3a14a2e08f5178e9989e4ecf81a753a6c1
Ravindra Kumar Saw <117341138+Ravindra9547@users.noreply.github.com>
2022-12-09 13:46:55 +0530

    Add files via upload
    Shopping Kart Project using array.

---
+++ 09-12-2022/ShoppingKart.java
@@ -0,0 +1,12 @@
+package shoppingKartusingarray;
+import java.util.Scanner;
+
+//this is main entry point
+public class ShoppingKart {
+static Scanner sc=new Scanner(System.in);
+public static void main(String []args)
+{
+	System.out.println("\t~~Welcome To Shopping Kart~~");
+	AdminAndUserPanel.AllOPerations();
+}
+}
