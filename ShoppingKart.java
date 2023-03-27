be25dca538395ed4e442d06dd3b1521cc19ff928
Ravindra <ravindrakumar880880@gmail.com>
2023-03-27 15:51:17 +0530

    commit

---
+++ ShoppingKart.java
@@ -0,0 +1,10 @@
+package shoppingkartusingarray;
+
+//this is main entry point
+public class ShoppingKart {
+public static void main(String []args)
+{
+	System.out.println("\t~~Welcome To Shopping Kart~~");
+	AdminAndUserPanel.AllOPerations();
+}
+}
